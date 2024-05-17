98e83d249292a6bc4ba3c49c50438bd936f6753a
Ordep769 <pedrohsdasilva769@gmail.com>
2024-05-16 21:53:05 -0300

    Final aula 5

---
+++ WF.Algoritmo.ClasseEmetodo/Form1.cs
@@ -0,0 +1,15 @@
+namespace WF.Algoritmo.ClasseEmetodo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void foreverCheckBox1_CheckedChanged(object sender)
+        {
+
+        }
+    }
+}
